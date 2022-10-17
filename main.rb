c02abbac30ba7a3091ba55451e9c663e220c49b6
hugo-pb <h.palomerabecerra@gmail.com>
2022-10-17 14:28:39 -0600

    games plays starts and ends

--- main.rb
+++ main.rb
@@ -8,14 +8,27 @@ def start
 player1 = Player.new('player1')
 player2 = Player.new('player2')
 
-puts " player 1 lives:#{player1.see_Lives}/3" 
-puts " player 2 lives:#{player2.see_Lives}/3" 
+
 
 game = Game.new(player1)
-puts game.player?
-
+i = 0
 ## loop questions and alternate between players || stop loop is lives = 0 
+while (player1.see_Lives.to_i > 0  ) do
+  puts game.player?
+  puts "question #{i}"
+  i = i + 1
+  
+  print "> "
+ answer = $stdin.gets.chomp
+p "your answer #{answer} "
 
+player1.minus_lives
+
+  puts " player 1 lives:#{player1.see_Lives}/3" 
+puts " player 2 lives:#{player2.see_Lives}/3" 
+
+end
+p 'end game player 2 wins'
 ## check if answer is correct - lives if not
 
 end
--- players.rb
+++ players.rb
@@ -11,6 +11,11 @@ attr_accessor :name
   def see_Lives
     self.lives
   end
+
+  def minus_lives
+    self.lives = self.lives - 1
+  end
+
   def to_s
     self.name
   end
